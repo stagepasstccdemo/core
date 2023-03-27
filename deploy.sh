5e94c4a2534f67bd0f4fdaf0de9d8cfc407f28be
fillipeags <fillipe484@gmail.com>
2023-03-26 23:44:16 -0300

    chore: setup initial deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ yarn build
 # Deploy to S3
 aws s3 cp s3://mfe-stage-pass/config/import-map.json import-map.json
 NEW_URL=/config/mfe/root-config/$VERSION/stagepass-root-config.js
-cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/root-config"] = $NEW_URL' > new.importmap.json
+cat ./import-map.json | jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/root-config"] |= $NEW_URL' > new.importmap.json
 aws s3 cp dist s3://mfe-stage-pass/config/mfe/root-config/$VERSION --recursive
 aws s3 cp dist/index.html s3://mfe-stage-pass/index.html
 aws s3 cp new.importmap.json s3://mfe-stage-pass/config/import-map.json
