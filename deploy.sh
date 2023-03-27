a38c6a7653a8f387e4c58d82d796ea804b6d5890
fillipeags <fillipe484@gmail.com>
2023-03-27 00:19:17 -0300

    chore: setup initial deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,5 @@ cat ./import-map.json | jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/root-c
 aws s3 cp dist s3://mfe-stage-pass/config/mfe/root-config/$VERSION --recursive
 aws s3 cp dist/index.html s3://mfe-stage-pass/index.html
 aws s3 cp new.importmap.json s3://mfe-stage-pass/config/import-map.json
-aws cloudfront create-invalidation --distribution-id ELQLVB5EJD6XG --paths '/config/import-map.json'
+aws cloudfront create-invalidation --distribution-id ER2L09XX3DDC7 --paths '/config/import-map.json'
+
