f01b96b4f8c28841ed2e6a0a9df737eebb419990
fillipeags <fillipe484@gmail.com>
2023-03-28 16:46:58 -0300

    chore: testing deleting old versions on aws s3 bucket

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ aws s3 cp s3://mfe-stage-pass/config/import-map.json import-map.json
 NEW_URL=/config/mfe/root-config/$VERSION/stagepass-root-config.js
 cat ./import-map.json | ./jq --arg NEW_URL "$NEW_URL" '.imports["@stagepass/root-config"] = $NEW_URL' > new.importmap.json
 aws s3 cp dist s3://mfe-stage-pass/config/mfe/root-config/$VERSION --recursive
+aws s3 ls s3://mfe-stage-pass/config/mfe/root-config/ | awk '{print $2}' | sort -r | tail -n +3 | xargs -I {} aws s3 rm s3://mfe-stage-pass/config/mfe/root-config/{} --recursive
 aws s3 cp dist/index.html s3://mfe-stage-pass/index.html
 aws s3 cp dist/public s3://mfe-stage-pass/public --recursive
 aws s3 cp dist/service-worker.js s3://mfe-stage-pass/service-worker.js
