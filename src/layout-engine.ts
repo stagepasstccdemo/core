1948f1c73f5f1c649da6dbd422405bffc9d52f79
fillipeags <fillipe484@gmail.com>
2023-03-28 16:32:54 -0300

    chore: setup sentry monitoring for core application

--- src/layout-engine.ts
+++ src/layout-engine.ts
@@ -6,6 +6,9 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
 const routes = constructRoutes(microfrontendLayout);
 
 const applications = constructApplications({
@@ -19,3 +22,9 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 applications.forEach(registerApplication);
 layoutEngine.activate();
 start();
+
+Sentry.init({
+  dsn: "https://094713d97e6c445abd8e7642f84dd0fb@o4504899977936896.ingest.sentry.io/4504917238743040",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
