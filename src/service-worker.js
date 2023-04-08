03720d9153918a528db85f228c7c202afcdcfd07
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-08 02:03:48 -0300

    chore: updating pwa settings and optimizing seo

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,25 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
 );
 
+const CACHE_NAME = "@stagepass/cache";
+
+const { precacheAndRoute } = workbox.precaching;
+
+precacheAndRoute([{ url: "/index.html", revision: "1" }]);
+
+const { registerRoute } = workbox.routing;
+
+const { CacheFirst, NetworkFirst, StaleWhileRevalidate } = workbox.strategies;
+const { CacheableResponsePlugin } = workbox.cacheableResponse;
+
 workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst()
+  ({ url }) => url.origin === "https://stagepasstickets.com",
+  new NetWorkFirst({
+    cacheName: CACHE_NAME,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
